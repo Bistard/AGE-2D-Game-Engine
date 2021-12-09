a3d1f17284dfbe615913042ecaf5522b95a1534a
Bistard <lshh1015813038@gmail.com>
2021-12-09 23:59:11 +0800

    [FIX] minor compile error on `UUID`

--- src/utils/uuid.cpp
+++ src/utils/uuid.cpp
@@ -5,8 +5,9 @@
 namespace AGE
 {
 
+/** @brief generate random number stuff */
 static std::random_device s_randomDevice;
-static std::mt19937_64 s_engine(s_randomDevice);
+static std::mt19937_64 s_engine( s_randomDevice() );
 static std::uniform_int_distribution<uint64_t> s_uniformDistribution;
 
 UUID::UUID(): _UUID { s_uniformDistribution(s_engine) } 
